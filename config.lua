1930e901523436a7851043d3f909f43dc3db56a1
b <b@aozsky.com>
2023-02-12 01:50:26 +0800

    format config file

--- config.lua
+++ config.lua
@@ -10,9 +10,9 @@ vim.opt.relativenumber = true
 -- general
 lvim.log.level = "info"
 lvim.format_on_save = {
-    enabled = true,
-    pattern = "*.lua",
-    timeout = 1000,
+	enabled = true,
+	pattern = "*.lua",
+	timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -29,20 +29,20 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["m"] = {
-    m = { "<cmd>SymbolsOutline<CR>", "SymbolsOutline" },
-    g = { "<cmd>GraphvizCompile<CR>", "GraphvizCompile" },
+	m = { "<cmd>SymbolsOutline<CR>", "SymbolsOutline" },
+	g = { "<cmd>GraphvizCompile<CR>", "GraphvizCompile" },
 }
 lvim.builtin.which_key.mappings["o"] = {
-    name = "octo",
-    i = { "<cmd>Octo issue list<CR>", "List all issues" },
+	name = "octo",
+	i = { "<cmd>Octo issue list<CR>", "List all issues" },
 }
 lvim.builtin.which_key.mappings["t"] = {
-    name = "Translate",
-    t = { "<cmd>Translate zh<CR>", "Translate" },
-    r = { "<cmd>Translate en -output=replace<CR>", "Translate to English and replace" },
-    c = { "<cmd>Translate zh -output=replace<CR>", "Translate to Chinese and replace" },
-    b = { "<cmd>ToggleTerm<CR>", "Terminal" },
-    p = { "<cmd>Telescope projects<CR>", "Projects" }
+	name = "Translate",
+	t = { "<cmd>Translate zh<CR>", "Translate" },
+	r = { "<cmd>Translate en -output=replace<CR>", "Translate to English and replace" },
+	c = { "<cmd>Translate zh -output=replace<CR>", "Translate to Chinese and replace" },
+	b = { "<cmd>ToggleTerm<CR>", "Terminal" },
+	p = { "<cmd>Telescope projects<CR>", "Projects" },
 }
 
 -- -- Change theme settings
@@ -98,17 +98,18 @@ lvim.builtin.treesitter.auto_install = true
 --   },
 -- }
 
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---     {command = "goimports", filetypes = {"go"}},
---     {command = "gofumpt", filetypes = {"go"}},
---     {command = "lua-format", filetypes = {"lua"}},
---     {command = "stylua", filetypes = {"lua"}},
---     {command = "prettier", filetypes= {"dot"}},
--- }
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "goimports", filetypes = { "go" } },
+	{ command = "gofumpt", filetypes = { "go" } },
+	-- { command = "lua-format", filetypes = { "lua" } },
+	{ command = "stylua", filetypes = { "lua" } },
+	{ command = "prettier", filetypes = { "dot" } },
+})
+lvim.format_on_save = { pattern = { "*.go" } }
+-- lvim.format_on_save = {pattern = {"*.go"}}
+lvim.format_on_save = true
 
--- lvim.format_on_save = {pattern = {"*.go"}}
--- lvim.format_on_save = {pattern = {"*.go"}}
 -- local linters = require "lvim.lsp.null-ls.linters"
 -- linters.setup {
 --   { command = "flake8", filetypes = { "python" } },
@@ -126,103 +127,105 @@ lvim.builtin.treesitter.auto_install = true
 --     },
 -- }
 lvim.plugins = {
-    {
-        -- "folke/tokyonight.nvim",
-    },
-    {
-        "tpope/vim-surround",
-    },
-    {
-        "christoomey/vim-tmux-navigator",
-    },
-    {
-        "rescript-lang/vim-rescript",
-        "nkrkv/nvim-treesitter-rescript",
-    },
-    {
-        "simrat39/symbols-outline.nvim",
-        config = function()
-          require('symbols-outline').setup({
-              position = 'left',
-              relative_width = true
-          })
-        end,
-        cmd = "SymbolsOutline"
-    },
-    {
-        'uga-rosa/translate.nvim'
-        -- 'uga-rosa/cmp-dictionary'
-    },
-    {
-        "folke/todo-comments.nvim",
-        event = "BufRead",
-        config = function()
-          require("todo-comments").setup()
-        end,
-    },
-    {
-        "olexsmir/gopher.nvim",
-    },
-    {
-        'edolphin-ydf/goimpl.nvim',
-        -- requires = {
-        dependencies = {
-            { 'nvim-lua/plenary.nvim' }, { 'nvim-lua/popup.nvim' },
-            { 'nvim-telescope/telescope.nvim' },
-            { 'nvim-treesitter/nvim-treesitter' }
-
-        },
-        config = function() require 'telescope'.load_extension 'goimpl' end
-    },
-    {
-        "fatih/vim-go",
-    },
-    {
-        'liuchengxu/graphviz.vim',
-    },
-    {
-        "ellisonleao/gruvbox.nvim"
-    },
-    {
-        "brymer-meneses/grammar-guard.nvim",
-        -- requires = {
-        dependencies = {
-            "neovim/nvim-lspconfig",
-            "williamboman/nvim-lsp-installer"
-        }
-    },
-    -- {
-    --     "bantana/vim-present",
-    -- },
-    {
-        'pwntester/octo.nvim',
-        config = function()
-          require "octo".setup()
-        end
-    }
+	{
+		-- "folke/tokyonight.nvim",
+	},
+	{
+		"tpope/vim-surround",
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+	},
+	{
+		"rescript-lang/vim-rescript",
+		"nkrkv/nvim-treesitter-rescript",
+	},
+	{
+		"simrat39/symbols-outline.nvim",
+		config = function()
+			require("symbols-outline").setup({
+				position = "left",
+				relative_width = true,
+			})
+		end,
+		cmd = "SymbolsOutline",
+	},
+	{
+		"uga-rosa/translate.nvim",
+		-- 'uga-rosa/cmp-dictionary'
+	},
+	{
+		"folke/todo-comments.nvim",
+		event = "BufRead",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{
+		"olexsmir/gopher.nvim",
+	},
+	{
+		"edolphin-ydf/goimpl.nvim",
+		-- requires = {
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-lua/popup.nvim" },
+			{ "nvim-telescope/telescope.nvim" },
+			{ "nvim-treesitter/nvim-treesitter" },
+		},
+		config = function()
+			require("telescope").load_extension("goimpl")
+		end,
+	},
+	{
+		"fatih/vim-go",
+	},
+	{
+		"liuchengxu/graphviz.vim",
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+	},
+	{
+		"brymer-meneses/grammar-guard.nvim",
+		-- requires = {
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"williamboman/nvim-lsp-installer",
+		},
+	},
+	-- {
+	--     "bantana/vim-present",
+	-- },
+	{
+		"pwntester/octo.nvim",
+		config = function()
+			require("octo").setup()
+		end,
+	},
 }
 require("grammar-guard").init()
 
-require("lspconfig").ltex.setup {
-    -- cmd = { '/usr/local/bin/ltex-ls' },
-    settings = {
-        ltex = {
-            enabled = { "latex", "tex", "bib", "markdown", "dot", "slide", "octo" },
-            language = "en",
-            diagnosticSeverity = "information",
-            setenceCacheSize = 2000,
-            additionalRules = {
-                enablePickyRules = true,
-                motherTongue = "en",
-            },
-            trace = { server = "verbose" },
-            -- dictionary = { ['en-US'] = { 'perf', 'ci' }, },
-            dictionary = {},
-            disabledRules = {},
-            hiddenFalsePositives = {},
-        },
-    },
-}
+require("lspconfig").ltex.setup({
+	-- cmd = { '/usr/local/bin/ltex-ls' },
+	settings = {
+		ltex = {
+			enabled = { "latex", "tex", "bib", "markdown", "dot", "slide", "octo" },
+			language = "en",
+			diagnosticSeverity = "information",
+			setenceCacheSize = 2000,
+			additionalRules = {
+				enablePickyRules = true,
+				motherTongue = "en",
+			},
+			trace = { server = "verbose" },
+			-- dictionary = { ['en-US'] = { 'perf', 'ci' }, },
+			dictionary = {},
+			disabledRules = {},
+			hiddenFalsePositives = {},
+		},
+	},
+})
 
 require("lvim.lsp.manager").setup("emmet_ls")
 
@@ -230,58 +233,58 @@ lvim.builtin.terminal.active = true
 lvim.builtin.terminal.shell = "bash --rcfile ~/.profile"
 
 require("translate").setup({
-    defalut = {
-        command = "translate_shell",
-        output = "floating",
-        relative_width = 25,
-    },
-    preset = {
-        output = {
-            insert = {
-                base = "top",
-                off = -1,
-            },
-        },
-    },
+	defalut = {
+		command = "translate_shell",
+		output = "floating",
+		relative_width = 25,
+	},
+	preset = {
+		output = {
+			insert = {
+				base = "top",
+				off = -1,
+			},
+		},
+	},
 })
 
 local status_ok, gopher = pcall(require, "gopher")
 if not status_ok then
-  return
+	return
 end
 
-gopher.setup {
-    commands = {
-        go = "go",
-        gomodifytags = "gomodifytags",
-        gotests = "gotests",
-        impl = "goimpl",
-        iferr = "iferr",
-    },
-}
+gopher.setup({
+	commands = {
+		go = "go",
+		gomodifytags = "gomodifytags",
+		gotests = "gotests",
+		impl = "goimpl",
+		iferr = "iferr",
+	},
+})
 
 lvim.builtin.which_key.mappings["G"] = {
-    name = "Go",
-    a = { "<cmd>GoAlternate<CR>", "GoAlternate" },
-    i = { "<cmd>lua require'telescope'.extensions.goimpl.goimpl{}<CR>", "GoImpl" },
-    I = { "<cmd>GoInstallDeps<cr>", "Install Go Dependencies" },
-    m = { "<cmd>GoMod tidy<cr>", "Tidy" },
-    t = {
-        name = "Test",
-        a = { "<cmd>GoAlternate<cr>", "Add Test" },
-        s = { "<cmd>GoTestsAll<cr>", "Add All Tests" },
-        e = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
-        f = { "<cmd>GoTestFunc<CR>", "GoTestFunc" }
-    },
-    -- g = {
-    --   name = "Generate",
-    --   -- c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
-    --   -- f = { "<cmd>GoGenerate %<cr>", "Go Generate File" },
-    --   -- g = { "<cmd>GoGenerate<cr>", "Go Generate" },
-    -- },
-    d = { "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test" },
-    e = { "<cmd>GoIfErr<cr>", "GoIfErr" },
-    s = { "<cmd>IndentBlanklineToggle<cr>", "IndentBlanklineToggle" },
+	name = "Go",
+	a = { "<cmd>GoAlternate<CR>", "GoAlternate" },
+	i = { "<cmd>lua require'telescope'.extensions.goimpl.goimpl{}<CR>", "GoImpl" },
+	I = { "<cmd>GoInstallDeps<cr>", "Install Go Dependencies" },
+	m = { "<cmd>GoMod tidy<cr>", "Tidy" },
+	t = {
+		name = "Test",
+		a = { "<cmd>GoAlternate<cr>", "Add Test" },
+		s = { "<cmd>GoTestsAll<cr>", "Add All Tests" },
+		e = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
+		f = { "<cmd>GoTestFunc<CR>", "GoTestFunc" },
+	},
+	-- g = {
+	--   name = "Generate",
+	--   -- c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
+	--   -- f = { "<cmd>GoGenerate %<cr>", "Go Generate File" },
+	--   -- g = { "<cmd>GoGenerate<cr>", "Go Generate" },
+	-- },
+	d = { "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test" },
+	e = { "<cmd>GoIfErr<cr>", "GoIfErr" },
+	s = { "<cmd>IndentBlanklineToggle<cr>", "IndentBlanklineToggle" },
 }
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
@@ -292,16 +295,14 @@ lvim.builtin.which_key.mappings["G"] = {
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
-vim.api.nvim_set_keymap('i', 'jj',
-    "<ESC>",
-    { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true, silent = true })
 
-vim.g.graphviz_viewer = 'open'
+vim.g.graphviz_viewer = "open"
 -- Options passed on to dot. Default is ''.
-vim.g.graphviz_shell_option = ''
+vim.g.graphviz_shell_option = ""
 -- `.format` is 'pdf' by default.
 -- Option: 'ps', 'pdf', 'png', 'jpg', 'gif', 'svg'
-vim.g.graphviz_output_format = 'svg'
+vim.g.graphviz_output_format = "svg"
 
 vim.opt.termguicolors = true
 lvim.builtin.indentlines.options.enabled = false
@@ -314,7 +315,7 @@ lvim.reload_config_on_save = false
 vim.opt.spell = true
 vim.opt.spelllang = { "en" }
 vim.filetype.add({
-    extension = {
-        slide = "markdown"
-    }
+	extension = {
+		slide = "markdown",
+	},
 })
